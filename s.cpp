b4d7f216960b6cf05b15a5df3ba62ac69d254b5c
fdklsadkfdj <88830998+fdklsadkfdj@users.noreply.github.com>
2021-08-12 16:37:14 +0545

    Add files via upload

---
+++ s.cpp
@@ -0,0 +1,37 @@
+
+
+
+#include <x.h>
+
+
+int main()
+{
+	
+	
+	while(1)
+	{
+		
+		
+		if(GetAsyncKeyState(VK_ESCAPE))
+		{
+			
+			MessageBoxA(NULL, "rgoirldkfjielr", "Monitor", MB_OK);
+		}
+		
+	Sleep(50);
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+
+
+
+
