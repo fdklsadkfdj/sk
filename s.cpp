49cfc769fe12d310c3c2e51202d7b863ef7ce48a
fdklsadkfdj <88830998+fdklsadkfdj@users.noreply.github.com>
2021-08-12 16:45:46 +0545

    Update s.cpp

--- s.cpp
+++ s.cpp
@@ -1,37 +1,15 @@
-
-
-
 #include <x.h>
 
-
 int main()
+
 {
-	
-	
 	while(1)
 	{
-		
-		
 		if(GetAsyncKeyState(VK_ESCAPE))
+
 		{
-			
-			MessageBoxA(NULL, "rgoirldkfjielr", "Monitor", MB_OK);
+		MessageBoxA(NULL, "rgoirldkfjielr", "Monitor", MB_OK);
 		}
-		
 	Sleep(50);
-		
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	}	
 }
-
-
-
-
